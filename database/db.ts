76f49fad97b874c25de3cc9dce42d40008b44196
Stefano Putelli <putellistefano@gmail.com>
2023-05-08 14:15:03 +0200

    added mongoose

--- database/db.ts
+++ database/db.ts
@@ -3,27 +3,17 @@ import { MongoClient, ServerApiVersion } from 'mongodb';
 import mongoose from 'mongoose';
 
 export default class DB {
-    client = null;
+    db_link = null;
     constructor(username : string, password : string) {
-        const uri = "mongodb+srv://" + username + ":" + password + "@bookbuddy.lcnowqs.mongodb.net/?retryWrites=true&w=majority";
-        this.client = new MongoClient(uri, {
-            serverApi: {
-                version: ServerApiVersion.v1,
-                strict: true,
-                deprecationErrors: true,
-            }
-        });
+        this.db_link = "mongodb+srv://" + username + ":" + password + "@bookbuddy.lcnowqs.mongodb.net/?retryWrites=true&w=majority";
     }
     async run() {
         try {
-            // Connect the client to the server	(optional starting in v4.7)
-            await this.client.connect();
-            // Send a ping to confirm a successful connection
-            await this.client.db("admin").command({ ping: 1 });
+            await mongoose.connect(this.db_link);
             console.log("Pinged your deployment. You successfully connected to MongoDB!");
         } finally {
-            // Ensures that the client will close when you finish/error
-            await this.client.close();
+            await mongoose.connection.close();
+            console.log("Closed connection to MongoDB.");
         }
     }
 }
