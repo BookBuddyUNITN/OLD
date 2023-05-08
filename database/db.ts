bb59b2f894d336a59cea885e84430218eb62ba9d
Stefano Putelli <putellistefano@gmail.com>
2023-05-08 14:28:31 +0200

    added example

--- database/db.ts
+++ database/db.ts
@@ -1,19 +1,11 @@
-import { MongoClient, ServerApiVersion } from 'mongodb';
-
 import mongoose from 'mongoose';
 
 export default class DB {
     db_link = null;
-    constructor(username : string, password : string) {
+    constructor(username: string, password: string) {
         this.db_link = "mongodb+srv://" + username + ":" + password + "@bookbuddy.lcnowqs.mongodb.net/?retryWrites=true&w=majority";
     }
-    async run() {
-        try {
-            await mongoose.connect(this.db_link);
-            console.log("Pinged your deployment. You successfully connected to MongoDB!");
-        } finally {
-            await mongoose.connection.close();
-            console.log("Closed connection to MongoDB.");
-        }
+    async connect() {
+        await mongoose.connect(this.db_link);
     }
 }
--- index.ts
+++ index.ts
@@ -1,11 +1,44 @@
 import DB from "./database/db"
 import * as dotenv from 'dotenv'
 
+import { Schema, model } from 'mongoose';
+
 const envs = dotenv.config()
 
-const db = new DB("backEnd",envs.parsed.MONGO_PASS)
+const db = new DB("backEnd", envs.parsed.MONGO_PASS)
 
-db.run().catch(console.dir);
+
+// 1. Create an interface representing a document in MongoDB.
+interface IUser {
+    name: string;
+    email: string;
+    avatar?: string;
+}
+
+// 2. Create a Schema corresponding to the document interface.
+const userSchema = new Schema<IUser>({
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    avatar: String
+});
+
+// 3. Create a Model.
+const User = model<IUser>('User', userSchema);
+
+
+
+db.connect().then(async () => {
+    const user = new User({
+        name: 'Bill',
+        email: 'bill@initech.com',
+        avatar: 'https://i.imgur.com/dM7Thhn.png'
+    });
+    await user.save();
+
+    console.log(user.email);
+}).catch((err) => {
+    console.log("Error connecting to MongoDB: " + err)
+})
 
 
 
