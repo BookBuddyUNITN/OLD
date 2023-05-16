bcc6e6c155fe644c057bf873316733f44410fc97
Stefano Putelli <putellistefano@gmail.com>
2023-05-16 21:39:11 +0200

    cors enabled

--- server/app.ts
+++ server/app.ts
@@ -1,8 +1,9 @@
 import express from "express"
-import jwt from "jsonwebtoken"
 import authRouter from "./routes/authRoutes"
 import libriRouter from "./routes/libriRoutes"
 import wishlistRoutes from "./routes/wishlistRouters"
+
+import cors from "cors"
 
 import tokenChecker from "./middleware/tokenChecker"
 
@@ -17,6 +18,9 @@ const app = express()
 export default function runServer() {
     app.use(express.json())
     app.use(express.urlencoded({ extended: true }))
+    app.use(cors())
+
+
     
     
     app.use(express.json())
